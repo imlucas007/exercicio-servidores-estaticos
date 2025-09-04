4c8a3625d09fb6938ac7f8125a907f90a22b19f8
Lucas Simões <lc.simoes@outlook.com>
2025-09-04 11:43:12 -0300

    Exibindo no HTML

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 let button = document.getElementById('btnUsers');
+let div = document.getElementById('out');
 
 button.addEventListener('click', function(){
     //Assincrona
-fetch('https://jsonplaceholder.typicode.com/posts/1')
+fetch('https://jsonplaceholder.typicode.com/posts/')
   .then(function(response){
-    return response;
+    return response.json();
   })
-  .then(function(json){
-    console.log(json);
+  .then(function(data){
+    div.textContent = `Title: ${data[0].title}, Body: ${data[0].body}`;
+    // let listaOrdenada = '<ol>';
+    
+    // for (usuario in data) {
+    //     '<li>' + `Title ${usuario.title}` +'</li>';
+    // }
   })
-    // console.log('Clicou');
 });
