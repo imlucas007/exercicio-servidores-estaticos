766f9c42d5fa0f6ffd69f85a1c34446980378887
Lucas Simões <lc.simoes@outlook.com>
2025-09-04 11:15:46 -0300

    Alterando fetch para função

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ let button = document.getElementById('btnUsers');
 button.addEventListener('click', function(){
     //Assincrona
 fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+  .then(function(response){
+    return response;
+  })
+  .then(function(json){
+    console.log(json);
+  })
     // console.log('Clicou');
 });
-
-
