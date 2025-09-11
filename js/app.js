94f808ba21426ec39d376967ab65494ec7c2793f
Lucas Simões <lc.simoes@outlook.com>
2025-09-11 11:34:37 -0300

    Adicionando throw e catch

--- js/app.js
+++ js/app.js
@@ -5,13 +5,20 @@ button.addEventListener('click', function(){
     //Assincrona
 fetch('https://jsonplaceholder.typicode.com/posts/99')
   .then(function(response){
-    return response.json();
+    // return response.json();
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Erro');
+    }
   })
   .then(function(json){
     let post = Post.frontRaw(json);
     div = post.renderFrom(div);
-    
-    // div.innerText = json.body;
-    // div.innerText = JSON.stringify(json, null, 0);
   })
+
+  .catch(function (erro) {
+    console.log(erro);
+    
+  });
 });
