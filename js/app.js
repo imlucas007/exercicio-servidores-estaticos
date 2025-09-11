e95704f7a9ff108a04c59d4e14eec450d9e4d3f4
Lucas Simões <lc.simoes@outlook.com>
2025-09-11 08:29:43 -0300

    Organizando projeto e adiconando Post.js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let button = document.getElementById('btnUsers');
+let div = document.getElementById('out');
+
+button.addEventListener('click', function(){
+    //Assincrona
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    div.innerText = json.body;
+    // div.innerText = JSON.stringify(json, null, 0);
+  })
+});
