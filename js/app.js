bc00a8000540d6dd2e2fafb4a17ad585fed0f4aa
Lucas Simões <lc.simoes@outlook.com>
2025-09-11 09:39:13 -0300

    Com id 99

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let div = document.getElementById('out');
 
 button.addEventListener('click', function(){
     //Assincrona
-fetch('https://jsonplaceholder.typicode.com/posts/1')
+fetch('https://jsonplaceholder.typicode.com/posts/99')
   .then(function(response){
     return response.json();
   })
