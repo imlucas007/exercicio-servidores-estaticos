e1ac0cced55726ac32d3784bc9527233bab8362f
Lucas Simões <lc.simoes@outlook.com>
2025-09-11 11:43:23 -0300

    Exibindo mensagem de erro

--- js/app.js
+++ js/app.js
@@ -3,22 +3,35 @@ let div = document.getElementById('out');
 
 button.addEventListener('click', function(){
     //Assincrona
-fetch('https://jsonplaceholder.typicode.com/posts/99')
+fetch('https://jsonplaceholder.typicode.com/posts/101')
   .then(function(response){
     // return response.json();
+
     if (response.ok) {
       return response.json();
     } else {
-      throw new Error('Erro');
+      let errorMessage = ('Recurso não encontrado ou servidor não disponível. Tente novamente mais tarde.');
+      throw new Error(errorMessage);
     }
+
   })
   .then(function(json){
     let post = Post.frontRaw(json);
     div = post.renderFrom(div);
   })
 
-  .catch(function (erro) {
-    console.log(erro);
+  .catch(function (error) {
+    // console.log(error);
+    renderError(error);
     
   });
+
 });
+      function renderError (error) {
+        div.innerHTML = "";
+
+        let h2 = document.createElement('h2');
+        h2.textContent = error;
+
+        div.appendChild(h2);
+    };
